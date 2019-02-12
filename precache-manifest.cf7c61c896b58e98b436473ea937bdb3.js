9db09da87c078373adc287a7bd244e558d28176f
Daniele I <irsooti@gmail.com>
2019-02-12 14:26:38 +0100

    Updates

---
+++ precache-manifest.cf7c61c896b58e98b436473ea937bdb3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e651e5f9b12ab51568b2",
+    "url": "/static/js/main.e651e5f9.chunk.js"
+  },
+  {
+    "revision": "7f7ff998227c62abd55e",
+    "url": "/static/js/2.7f7ff998.chunk.js"
+  },
+  {
+    "revision": "397c097656ebcbba449f9eeed0e9cdbb",
+    "url": "/index.html"
+  }
+];
